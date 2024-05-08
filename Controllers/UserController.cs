575f17d6f0128eea060b4b257cf285b390bebb91
github_cloudlabsuser_705@cloudlabsaiuser.com <github_cloudlabsuser_705@cloudlabsaiuser.com>
2024-05-08 01:32:35 +0000

    Updated USerController - Added Deploy files

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using CRUD_application_2.Models;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Web.Mvc;
  
 namespace CRUD_application_2.Controllers
@@ -10,19 +11,23 @@ namespace CRUD_application_2.Controllers
         // GET: User
         public ActionResult Index()
         {
-            // Implement the Index method here
+            // Implement the index method here
+            return View(userlist);
         }
- 
+
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            // Implement the details method
+            return View(userlist[id]);
         }
  
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+            userlist.Add(new User());
+            return View(userlist[userlist.Count - 1]);
         }
  
         // POST: User/Create
@@ -30,6 +35,12 @@ namespace CRUD_application_2.Controllers
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+            if (ModelState.IsValid)
+            {
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
  
         // GET: User/Edit/5
@@ -37,8 +48,17 @@ namespace CRUD_application_2.Controllers
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
+            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+
+            User user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, User user)
@@ -48,12 +68,31 @@ namespace CRUD_application_2.Controllers
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+
+            if (ModelState.IsValid)
+            {
+                User existingUser = userlist.FirstOrDefault(u => u.Id == id);
+                if (existingUser == null)
+                {
+                    return HttpNotFound();
+                }
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
  
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            User user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
         // POST: User/Delete/5
@@ -61,6 +100,13 @@ namespace CRUD_application_2.Controllers
         public ActionResult Delete(int id, FormCollection collection)
         {
             // Implement the Delete method (POST) here
+            User user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            userlist.Remove(user);
+            return RedirectToAction("Index");
         }
     }
 }
